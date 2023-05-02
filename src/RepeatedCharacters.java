2605e14e3c2b254022ed7476d05c156d48c46ddf
Your Name <you@example.com>
2023-05-02 21:41:50 +0300

    solution repated characters with set

---
+++ src/RepeatedCharacters.java
@@ -0,0 +1,28 @@
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class RepeatedCharacters {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the line");
+        String str = scanner.nextLine().toLowerCase();
+        char[] charStr = str.toCharArray();
+        Set <Character> repetedChar = new HashSet<>();
+
+        for (int i = 0; i < charStr.length; i++){
+            for (int j = i + 1; j < charStr.length; j++){
+                if (charStr[i] == charStr[j]){
+                    repetedChar.add(charStr[i]);
+                }
+            }
+        }
+
+        System.out.println("Repeated characters:");
+        for (char c : repetedChar){
+            System.out.println(c);
+        }
+
+    }
+}
